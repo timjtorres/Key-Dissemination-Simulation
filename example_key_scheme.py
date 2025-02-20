85be4690aa95f2c48cfa20e2b5eb6f7b0018ebce
Tim Torres <timjtorres@gmail.com>
2025-02-19 22:38:33 -0500

    change way to import package

--- example_key_scheme.py
+++ example_key_scheme.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
-from network_algs import *
+import igraph as ig
 
+from network_algs import ShareKey
 
 def main():
 
--- network_algs/__init__.py
+++ network_algs/__init__.py
@@ -1,4 +1,4 @@
 # from .alt_path_primitive import *
 from .base_funcs import *
-from .ShareKey import *
-from .ShareSecret import *
+from .ShareKey import ShareKey
+from .ShareSecret import ShareSecret
